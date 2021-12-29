8145fbc83d55d3434746077db9c49b4205e1ba96
unknown <you@example.com>
2021-12-29 14:51:35 +0530

    Add swagger

--- work/SpringBootRESTWithSwagger/src/main/java/com/shad/SpringBootRest/SwaggerConfig.java
+++ work/SpringBootRESTWithSwagger/src/main/java/com/shad/SpringBootRest/SwaggerConfig.java
@@ -1,20 +1,31 @@
 package com.shad.SpringBootRest;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Component
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.shad.SpringBootRest.controller")).paths(regex("/api.*"))
+				.build();
 
-	// URL: http://localhost:9191/swagger-ui
+	}
 
-	
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
--- work/SpringBootRESTWithSwagger/src/main/java/com/shad/SpringBootRest/controller/CustomerController.java
+++ work/SpringBootRESTWithSwagger/src/main/java/com/shad/SpringBootRest/controller/CustomerController.java
@@ -4,11 +4,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shad.SpringBootRest.entity.Customer;
 import com.shad.SpringBootRest.service.CustomerService;
-
+@RequestMapping("/api")
 @RestController
 public class CustomerController {
 
