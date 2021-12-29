e93c605e4c5b6704c9322295612c66a4091a5c1a
unknown <you@example.com>
2021-12-29 16:43:02 +0530

    add FrontEnd

---
+++ FrontEnd/js/getTrainData.js
@@ -0,0 +1,48 @@
+
+const url = 'http://localhost:9191/api/train';
+
+async function callApi() {
+    let response = await fetch(url);
+    let jsonStr = await response.text();
+    console.log(jsonStr);
+
+    createTable(jsonStr);
+}
+
+function createTable(jsonStr) {
+
+    const jsonObj = JSON.parse(jsonStr);
+
+    //  const data = jsonObj.data;
+    const data = jsonObj;
+
+    const tableBody = document.getElementById('table_body')
+
+    for (let x in data) {
+
+        let obj = data[x];
+
+        const row = document.createElement('tr');
+
+        const cell1 = document.createElement('td');
+        cell1.innerHTML = obj.trainNo;
+        const cell2 = document.createElement('td');
+        cell2.innerHTML = obj.trainName;
+        const cell3 = document.createElement('td');
+        cell3.innerHTML = obj.source;
+        const cell4 = document.createElement('td');
+        cell3.innerHTML = obj.destination;
+        const cell5 = document.createElement('td');
+        cell3.innerHTML = obj.ticketPrice;
+
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        row.appendChild(cell3);
+        row.appendChild(cell4);
+        row.appendChild(cell5);
+
+        tableBody.appendChild(row);
+    }
+}
+
+callApi();
--- work/SpringBootRESTWithSwagger/src/main/java/com/shad/SpringBootRest/controller/TrainController.java
+++ work/SpringBootRESTWithSwagger/src/main/java/com/shad/SpringBootRest/controller/TrainController.java
@@ -1,9 +1,11 @@
 package com.shad.SpringBootRest.controller;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.shad.SpringBootRest.entity.Train;
 import com.shad.SpringBootRest.service.TrainService;
+
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/api")
 @RestController
 public class TrainController {
